dcfa71ef9667006d0e62b5bfe1897de7e62f125c
Maronato <gustavomaronato@gmail.com>
2016-11-07 17:27:35 -0500

    tempfix for import loop

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from misago.models import User
-from party.models import Members
+#from party.models import Members
 
 
 class Profile(models.Model):
@@ -14,12 +14,12 @@ class Profile(models.Model):
     # method to get all of the profile's votes in a specific poll
     # def curr_poll_votes(self, poll):
 
-    def make_member(self):
-        self.is_mod = True
-        self.save()
-        m = Members(profile=self)
-        m.save()
-        return True
+    # def make_member(self):
+    #     self.is_mod = True
+    #     self.save()
+    #     m = Members(profile=self)
+    #     m.save()
+    #     return True
 
     @staticmethod
     def user_from_ra(number):
